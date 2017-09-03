6ef930fa13ba9900ad49f0bd7b5dec79341203c6
Josh Crockett <josh.crockett@gmail.com>
2017-09-02 22:31:23 -0400

    Unit testing with rewind/ffwd

--- tester.py
+++ tester.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python
 
 from chalicelib.resetter import launch
+
+print "rewind:"
+print str(launch("WSH",rewind=True))
+print str(launch("SD",rewind=True))
+
+print "today:"
 print str(launch("WSH"))
 print str(launch("SD"))
+
+print "ffwd:"
+print str(launch("WSH",ffwd=True))
+print str(launch("SD",ffwd=True))
+
+print "bad team:"
 print str(launch("asdfas"))
