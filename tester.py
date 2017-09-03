9ee4a2579186e83e60b160c12c5524b5f36bf454
Josh Crockett <josh.crockett@gmail.com>
2017-09-02 23:34:13 -0400

    fixes and full unit test

--- tester.py
+++ tester.py
@@ -2,17 +2,25 @@
 
 from chalicelib.resetter import launch
 
-print "rewind:"
+print "Should see: standard, verbose for each game. 'Today' starts at rolloverTime.\n"
+
+print "yesterday:"
 print str(launch("WSH",rewind=True))
+print str(launch("WSH",fluidVerbose=True,rewind=True))
 print str(launch("SD",rewind=True))
+print str(launch("SD",fluidVerbose=True,rewind=True))
 
 print "today:"
 print str(launch("WSH"))
+print str(launch("WSH",fluidVerbose=True))
 print str(launch("SD"))
+print str(launch("SD",fluidVerbose=True))
 
-print "ffwd:"
+print "tomorrow:"
 print str(launch("WSH",ffwd=True))
+print str(launch("WSH",fluidVerbose=True,ffwd=True))
 print str(launch("SD",ffwd=True))
+print str(launch("SD",fluidVerbose=True,ffwd=True))
 
 print "bad team:"
 print str(launch("asdfas"))
