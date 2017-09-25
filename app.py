03b68e6ebfad833b3f8706133267a68c211a89b5
Josh Crockett <josh.crockett@gmail.com>
2017-09-24 21:23:19 -0400

    refactor

--- app.py
+++ app.py
@@ -15,13 +15,22 @@ app = Chalice(app_name='resetter')
 
 @app.route('/',methods=['GET'])
 def index():
+	"""Route used for Slack"""
 	try:
 		team = app.current_request.query_params['text']
 		if team == '':
 			team = DEFAULT_PARAM
 	except:
 		team = DEFAULT_PARAM
+
 	print "getting for team " + team
+	rtext = get_team(team)
+	
+	return { "response_type": "in_channel", "text": rtext }
+
+
+def get_team(team):
+	"Generic fetch prioritization"
 	
 	if team.strip().lower().endswith("football"):
 		retList = get_ncaaf(team[:-8].strip())
@@ -42,4 +51,4 @@ def index():
 	else:
 		rtext = join(retList," ")
 	
-	return { "response_type": "in_channel", "text": rtext }
+	return rtext
