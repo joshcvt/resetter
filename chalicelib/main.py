0513b1822304d38d8d55df92356d26e9676f7d69
Josh Crockett <josh.crockett@gmail.com>
2017-10-01 22:33:21 -0400

    add NHL

--- chalicelib/main.py
+++ chalicelib/main.py
@@ -4,19 +4,31 @@ from string import join
 
 from resetter import launch as get_mlb
 from ncaaf_ncaa import get as get_ncaaf
-
+from nhl import get as get_nhl
 
 def get_team(team):
 	"Generic fetch prioritization"
 	
-	if team.strip().lower().endswith("football"):
+	line = team.strip().lower()
+	
+	if line.endswith("football"):
 		retList = get_ncaaf(team[:-8].strip())
-				
+	elif line.endswith("hockey"):
+		retList = get_nhl(team[:-6].strip())
+	elif line.endswith("nhl"):
+		retList = get_nhl(team[:-3].strip())
+	elif line.endswith("mlb"):
+		retList = get_mlb(team[:-3].strip())
+	elif line.endswith("baseball"):
+		retList = get_mlb(team[:-8].strip())
 	else:	
 		retList = get_mlb(team,True)
 		if retList == None:
 			# try football?
 			retList = get_ncaaf(team)
+		if retList == None:
+			# how about hockey?
+			retList = get_nhl(team)
 	
 	if retList and (retList.__class__ != list):
 		retList = [retList]
