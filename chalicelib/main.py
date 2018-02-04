63664de1d070585a21dd118d35229bc4a129b49e
Josh Crockett <josh.crockett@gmail.com>
2018-02-04 15:32:49 -0500

    rm get_team_old

--- chalicelib/main.py
+++ chalicelib/main.py
@@ -6,56 +6,6 @@ from resetter import launch as get_mlb
 from ncaaf_ncaa import get as get_ncaaf
 from nhl import get as get_nhl
 from reset_lib import NoGameException	
-
-def get_team_old(team):
-	"Generic fetch prioritization"
-	
-	line = team.strip().lower()
-	
-	if line.endswith("football"):
-		retList = get_ncaaf(team[:-8].strip())
-	elif line.startswith("football "):
-		retList = get_ncaaf(team[9:].strip())
-	elif line.endswith("hockey"):
-		retList = get_nhl(team[:-6].strip())
-	elif line.startswith("hockey "):
-		retList = get_nhl(team[7:].strip())
-	elif line.endswith("nhl"):
-		retList = get_nhl(team[:-3].strip())
-	elif line.startswith("nhl "):
-		retList = get_nhl(team[4:].strip())
-	elif line.endswith("mlb"):
-		retList = get_mlb(team[:-3].strip())
-	elif line.startswith("mlb "):
-		retList = get_mlb(team[4:].strip())
-	elif line.endswith("baseball"):
-		retList = get_mlb(team[:-8].strip())
-	elif line.startswith("baseball "):
-		retList = get_mlb(team[9:].strip())
-	else:
-		retList = get_mlb(team,True)
-		if retList == None:
-			# try football?
-			retList = get_ncaaf(team)
-		if retList == None:
-			# how about hockey?
-			retList = get_nhl(team)
-	
-	if retList and (retList.__class__ != list):
-		retList = [retList]
-	
-	if retList == None:
-		if team.startswith("my "):
-			team = "your " + team[3:]
-		elif team.startswith("our "):
-			team = "your " + team[4:]
-		rtext = "I'm sorry, I can't reset " + team + "."
-	elif len(retList) == 1:
-		rtext = retList[0]
-	else:
-		rtext = join(retList," ")
-	
-	return rtext
 	
 def get_team(team):
 
