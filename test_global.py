ca5021853835443f6e32351d13d07cd54657b930
Josh Crockett <josh.crockett@gmail.com>
2020-08-15 20:53:23 -0400

    2to3

--- test_global.py
+++ test_global.py
@@ -4,14 +4,14 @@ from chalicelib.main import get_team
 
 teams = ["nhl toronto", "mlb toronto", "football miami", "Toronto","Washington","NY","NYR","NYY","LA","Miami","garbage","my aneurysm"]
 
-print "\n\n"
+print("\n\n")
 for t in teams:
-	print "Calling " + t 
+	print("Calling " + t) 
 	try:
 		rval = get_team(t,debug=True)
-		print "Final return: " + rval
+		print("Final return: " + rval)
 	except Exception as e:
-		print e.__class__.__name__, str(e)
-	print ""
+		print(e.__class__.__name__, str(e))
+	print("")
 
-print "Done!"
+print("Done!")
