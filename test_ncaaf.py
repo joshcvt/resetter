6f0d6df98f76c11fe667cb2815214b0fa4407775
Josh Crockett <josh.crockett@gmail.com>
2021-09-12 21:54:45 -0400

    roll test_ncaaf to py3

--- test_ncaaf.py
+++ test_ncaaf.py
@@ -3,24 +3,24 @@
 from chalicelib.ncaaf_ncaa import get
 from chalicelib.reset_lib import NoGameException
 
-print "Trying some teams that are likely to have midweek games, some early, and Hawaii (aloha).\n"
+print ("Trying some teams that are likely to have midweek games, some early, and Hawaii (aloha).\n")
 
 teams = ["Northern Ill.","Toledo","Western Michigan","MSU","VT","FSU","Tennessee","LSU","California","UCLA","Hawaii"]
 
 for t in teams:
 	try:
-		print get(t)
+		print( get(t))
 	except NoGameException as nge:
-		print str(nge)
+		print (str(nge))
 
-print "Now: I-AA, Unclear, Doesn't Exist:"
+print ("Now: I-AA, Unclear, Doesn't Exist:")
 
 teams = ["jmu","tigers","aasdfasdg"]
 
 for t in teams:
 	try:
-		print get(t)
+		print (get(t))
 	except NoGameException as nge:
-		print str(nge)
+		print (str(nge))
 
-print "Make sure a reload works (and dump exception directly if not): " + str(get("VT",forceReload=True))
+print ("Make sure a reload works (and dump exception directly if not): " + str(get("VT",forceReload=True)))
