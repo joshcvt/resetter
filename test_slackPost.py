3e6a48b9545507c5b316697fbfabb50010015c8c
Josh Crockett <josh.crockett@gmail.com>
2024-05-13 21:04:44 -0400

    slack test fix

--- test_slackPost.py
+++ test_slackPost.py
@@ -2,7 +2,7 @@
 
 from chalicelib.main import postSlack
 
-slacks = ["mlb 2024-04-06","nats 2024-04-06"]
+slacks = ["mlb 2024-09-06","nhl 2024-04-06"]
 print("")
 for slack in slacks:
 	print("posting Slack %s..." % (slack))
