8a54e0c0552e6ff6d2bdbc36d13e147e6ff44c17
Josh Crockett <josh.crockett@gmail.com>
2024-10-10 22:29:48 -0400

    Utah and postponed/cancelled fixes

--- test_nhl.py
+++ test_nhl.py
@@ -3,9 +3,11 @@
 from chalicelib.nhl import get
 from chalicelib.reset_lib import NoGameException
 
+from datetime import datetime
+
 print("Should see: full scoreboard, then a couple single games or attempts, for three days. 'Today' starts at rolloverTime.\n")
 
-testTeams = ["scoreboard","canes","canadiens","Washington","NYR"]
+testTeams = ["scoreboard","canes","Washington","utah"]
 # scoreboard for all, canes for lookup table, canadiens for direct team name
 
 print("yesterday:")
@@ -30,6 +32,15 @@ print("tomorrow:")
 for t in testTeams:
 	try:
 		print(str(get(t,ffwd=True)) + "\n")
+	except NoGameException:
+		print("No game for " + t)
+	except Exception as e:
+		print(str(e))
+
+print("hard date 2024-10-23 23:00 including a ppd:")
+for t in testTeams:
+	try:
+		print(str(get(t,date=datetime(2024,10,12,23,00))) + "\n")
 	except NoGameException:
 		print("No game for " + t)
 	except Exception as e:
